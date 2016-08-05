cdf6f3bf958a78ea6cf9ca71db305e1e8910668b
marissa0809 <renaorgani@gmail.com>
2016-08-05 21:44:51 +0000

    Finish routes

--- test/controllers/static_pages_controller.rb
+++ test/controllers/static_pages_controller.rb
@@ -1,14 +1,13 @@
-require 'test_helper'
-
-class StaticPagesControllerTest < ActionController::TestCase
+class StaticPagesController < ApplicationController
   test "should get home" do
     get :home
     assert_response :success
   end
-
+  
   test "should get help" do
     get :help
     assert_response :success
+ 
   end
-
+  
 end
